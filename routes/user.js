f9511ed99e261e111510b7b955586aefa166ae6f
mrutyunjayma <mrutyunjaymasanta05@gmail.com>
2024-07-24 22:01:21 +0530

    Finished all Backend

---
+++ routes/user.js
@@ -0,0 +1,64 @@
+import express from "express";
+import {
+  acceptFriendRequest,
+  getMyFriends,
+  getMyNotifications,
+  getMyProfile,
+  login,
+  logout,
+  newUser,
+  resetPassword,
+  searchUser,
+  sendFriendRequest,
+} from "../controllers/user.js";
+import {
+  acceptRequestValidator,
+  loginValidator,
+  registerValidator,
+  resetPasswordValidator,
+  sendRequestValidator,
+  validateHandler,
+} from "../lib/validators.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import { singleAvatar } from "../middlewares/multer.js";
+
+const app = express.Router();
+
+app.post("/new", singleAvatar, registerValidator(), validateHandler, newUser);
+app.post("/login", loginValidator(), validateHandler, login);
+app.post(
+  "/resetpassword",
+  resetPasswordValidator(),
+  validateHandler,
+  resetPassword
+);
+
+// After here user must be logged in to access the routes
+
+app.use(isAuthenticated);
+
+app.get("/me", getMyProfile);
+
+app.get("/logout", logout);
+
+app.get("/search", searchUser);
+
+app.put(
+  "/sendrequest",
+  sendRequestValidator(),
+  validateHandler,
+  sendFriendRequest
+);
+
+app.put(
+  "/acceptrequest",
+  acceptRequestValidator(),
+  validateHandler,
+  acceptFriendRequest
+);
+
+app.get("/notifications", getMyNotifications);
+
+app.get("/friends", getMyFriends);
+
+export default app;
